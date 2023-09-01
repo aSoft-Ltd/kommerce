3520825ee2ef2d787ccdb84177e3928214295f1e
andylamax <andylamax@programmer.net>
2023-09-01 13:31:33 +0300

    Manged to migrated sentinel into a composite build

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    includeBuild("../build-logic")
+}
+
+plugins {
+    id("multimodule")
+}
+
+fun includeSubs(base: String, path: String = base, vararg subs: String) {
+    subs.forEach {
+        include(":$base-$it")
+        project(":$base-$it").projectDir = File("$path/$it")
+    }
+}
+
+listOf(
+    "kommander", "kollections", "kevlar", "identifier"
+).forEach { includeBuild("../$it") }
+
+rootProject.name = "kommerce"
+
+includeSubs("kommerce", ".", "core")
