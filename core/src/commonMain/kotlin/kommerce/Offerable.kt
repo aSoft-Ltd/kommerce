a9241d6603e8364f0caade46e90d2d31db271003
andylamax <andylamax@programmer.net>
2023-12-11 16:59:54 +0300

    Adopting wasm

--- core/src/commonMain/kotlin/kommerce/Offerable.kt
+++ core/src/commonMain/kotlin/kommerce/Offerable.kt
@@ -6,7 +6,7 @@ package kommerce
 import identifier.Named
 import identifier.Unique
 import kotlinx.serialization.Serializable
-import kotlin.js.JsExport
+import kotlinx.JsExport
 
 @Serializable
 sealed interface Offerable : Unique, Named {
